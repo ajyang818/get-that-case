379a16c3dd7cf76b3cb0c91fd148c2aa0e012153
Allen Yang <allen@yipit.com>
2014-11-17 23:09:30 -0500

    Saving status; strange double listener problem

--- get-that-case-bundle/background_script.js
+++ get-that-case-bundle/background_script.js
@@ -1,7 +1,7 @@
 // Background script that receives a signal that the user clicked on "Order now", and then
 // pops open a new tab with the case request form. It will then try to fill in the form with the info it received.
 
-var logger = new Firebase("https://get-that-case.firebaseio.com/");
+// var logger = new Firebase("https://get-that-case.firebaseio.com/");
 
 chrome.runtime.onMessage.addListener(
 
@@ -19,17 +19,31 @@ chrome.runtime.onMessage.addListener(
     sendResponse({prodNum: request.prodNum, prodTitle: request.prodTitle});
 
     var requestFormUrl = 'https://inside.hbs.edu/Departments/KLS/Lists/HBP%20Case%20Request%20Form/Item/newifs.aspx';
+
+    // We get this script to listen for a signal that the newly opened tab (case request form) is ready.
+    // When we get that signal, this background script will send over the form data.
+
+    // debugger;
+    chrome.runtime.onConnect.addListener(function(port) {
+
+      console.assert(port.name == "formStatus");
+      port.onMessage.addListener(function(msg) {
+        if (msg.status == "formReady") {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {userEmail: hbsEmail, prodNum: request.prodNum, prodTitle: request.prodTitle, tabID: tabs[0].id}, function(response) {
+                // The below is being called twice
+                console.log("sent message for " + request.prodTitle + " to tab ID " + tabs[0].id);
+              });
+            });
+            // // Log usage data to Firebase
+            // var now = Date();
+            // logger.push({user: hbsEmail, prodNum: request.prodNum, prodTitle: request.prodTitle, timestamp: now});
+        }
+      });
+
+
+    });
+
     chrome.tabs.create({url: requestFormUrl});
-
-    function sendInfoToForm() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {userEmail: hbsEmail, prodNum: request.prodNum, prodTitle: request.prodTitle}, function(response) {
-          });
-        });
-        var now = Date();
-        logger.push({user: hbsEmail, prodNum: request.prodNum, prodTitle: request.prodTitle, timestamp: now});
-    }
-
-    window.setTimeout(sendInfoToForm, 1500);
 
 });
--- get-that-case-bundle/form_script.js
+++ get-that-case-bundle/form_script.js
@@ -3,6 +3,7 @@
 
 $(document).ready(function() {
 
+
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
@@ -12,5 +13,18 @@ $(document).ready(function() {
       $("input[originalid='V1_I1_T2']").val(request.prodNum);
       $("textarea[originalid='V1_I1_T3']").val(request.prodTitle);
 
+      // Log usage data to Firebase
+      var logger = new Firebase("https://get-that-case.firebaseio.com/");
+      var now = Date();
+      console.log("About to log " + request.prodTitle + "; I am tab ID " + request.tabID);
+      logger.push({user: request.userEmail, prodNum: request.prodNum, prodTitle: request.prodTitle, timestamp: now});
+
   });
+
+  // After the page is loaded and the listener is readied, we can tell
+  // the background script that we're ready to receive the form info
+  var port = chrome.runtime.connect({name: "formStatus"});
+  port.postMessage({status: "formReady"});
+  port.disconnect();
+
 });
