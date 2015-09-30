c012cc48510d238eaf81401af5f8164c3694d725
Allen Yang <allen@yipit.com>
2015-09-30 10:29:39 -0400

    Updating for new HBP website

--- get-that-case-bundle/hbp_script.js
+++ get-that-case-bundle/hbp_script.js
@@ -1,34 +1,33 @@
 // Content script that runs in the background when the user reaches the case search results page on the HBP website
 
-$(document).ready(function() {
+window.addEventListener("load", myMain, false);
 
-  $("tr.searchResult").each(function(index, resultBlock) {
-      var prodNums = $(this).find("dt:contains('Product #')");
-      if (prodNums.length == 1) {
-        var prodNumBlock = prodNums.next(),
-            prodNumText = prodNumBlock[0].textContent,
-            prodNum = prodNumText.replace("Product number:", ""),
-            prodTitle = $(this).find("p.title")[1].textContent;
-        // debugger;
-        $(this).find("ul.teaching-supplements").append("<li><a class='click-test' style='cursor:pointer' prodNum='" + prodNum + "' prodTitle=\"" + prodTitle + "\">Get That Case!</a></li>");
-      }
-  });
+function myMain (evt) {
+    var prodNums = $(this).find("dt:contains('Product #')");
+    var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
+    var jsPeriodicChecktimer = setInterval(checkForJS_Finish, 2000);
 
-});
+    function checkForJS_Finish () {
+        if (document.querySelector(".product-information") && !(document.querySelector('.gtc-link'))) {
+            clearInterval(jsInitChecktimer);
+            console.log("checkForJS_Finish ran");
+            $("li.hbp-region").each(function(index, resultBlock) {
+                var prodNumBlock = $(this).find(".productAvailabilityId"),
+                    prodNum = prodNumBlock[0].textContent,
+                    prodTitle = $(this).find(".title")[0].textContent;
+                $(this).find("div.content-links").append("<li><a class='gtc-link' style='cursor:pointer' prodNum='" + prodNum + "' prodTitle=\"" + prodTitle + "\">Get That Case!</a></li>");
+            });
+        }
 
-$(document).ready(function () {
-  $('.click-test').click(function() {
-    var prodNum = $(this)[0].getAttribute("prodNum"),
-        prodTitle = $(this)[0].getAttribute("prodTitle");
+        $(document).ready(function () {
+            $('.gtc-link').click(function() {
+                var prodNum = $(this)[0].getAttribute("prodNum"),
+                    prodTitle = $(this)[0].getAttribute("prodTitle");
+                chrome.runtime.sendMessage({prodNum: prodNum, prodTitle: prodTitle}, function(response) {
+                    // Activates popup.js, which is the background script, and passes along the relevant product number and title
+                });
+            });
+        });
 
-    chrome.runtime.sendMessage({prodNum: prodNum, prodTitle: prodTitle}, function(response) {
-      // Activates popup.js, which is the background script, and passes along the relevant product number and title
-    });
-  });
-});
-
-// window.addEventListener ("load", myMain, false);
-
-// function myMain (evt) {
-//   debugger;
-// }
+    }
+}
